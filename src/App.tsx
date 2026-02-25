1b1541c9deb1277387fb300e2e8c3e697aa155e4
Arthur Bicalho <arthurbic95@gmail.com>
2026-02-25 01:37:23 -0300

    nova rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "../src/pages/Menu/Menu";
 import MineralDetails from "../src/components/MineralDetails/MineralDetails";
 import "./App.css";
@@ -13,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/minerals/:id" element={<MineralDetails />} />
+        <Route path="/macro-minerals/:id" element={<MineralDetails />} />
       </Routes>
     </Router>
   );
